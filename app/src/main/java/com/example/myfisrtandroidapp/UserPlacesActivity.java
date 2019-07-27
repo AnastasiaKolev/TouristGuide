7907cd583e99ad307db8826ffeaf19cc6402a359
Anastasia <anastasia.kolevatykh@gmail.com>
2019-07-27 06:09:48 +0300

    user preferences added

--- app/src/main/java/com/example/myfisrtandroidapp/UserPlacesActivity.java
+++ app/src/main/java/com/example/myfisrtandroidapp/UserPlacesActivity.java
@@ -21,13 +21,11 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
 public class UserPlacesActivity extends AppCompatActivity {
     private final String TAG = "Chips Example";
-    private HashMap<String, Boolean> mPreferences = new HashMap<>();
     private ArrayList<String> aPreferences = new ArrayList<>();
     private Set<String> setPref = new HashSet<>();
 
@@ -42,15 +40,19 @@ public class UserPlacesActivity extends AppCompatActivity {
 
         mDb = FirebaseFirestore.getInstance();
 
-        mPreferences.put("museum", true);
-        mPreferences.put("church", false);
-        mPreferences.put("art_gallery", false);
-
         /*Filter Chip section*/
         ChipGroup filterChipGroup = findViewById(R.id.filter_chip_group);
         Chip museum_chip = findViewById(R.id.museum_chip);
         Chip church_chip = findViewById(R.id.church_chip);
         Chip art_gallery_chip = findViewById(R.id.art_gallery_chip);
+        Chip aquarium_chip = findViewById(R.id.aquarium_chip);
+        Chip zoo_chip = findViewById(R.id.zoo_chip);
+        Chip amusement_park_chip = findViewById(R.id.amusement_park_chip);
+        Chip city_hall_chip = findViewById(R.id.city_hall_chip);
+        Chip mosque_chip = findViewById(R.id.mosque_chip);
+        Chip park_chip = findViewById(R.id.park_chip);
+        Chip synagogue_chip = findViewById(R.id.synagogue_chip);
+
         CompoundButton.OnCheckedChangeListener filterChipListener = new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -64,26 +66,52 @@ public class UserPlacesActivity extends AppCompatActivity {
                 if (art_gallery_chip.isChecked()) {
                     setPref.add("art_gallery");
                 }
+                if (aquarium_chip.isChecked()) {
+                    setPref.add("aquarium_chip");
+                }
+                if (zoo_chip.isChecked()) {
+                    setPref.add("zoo_chip");
+                }
+                if (amusement_park_chip.isChecked()) {
+                    setPref.add("amusement_park_chip");
+                }
+                if (city_hall_chip.isChecked()) {
+                    setPref.add("city_hall_chip");
+                }
+                if (mosque_chip.isChecked()) {
+                    setPref.add("mosque_chip");
+                }
+                if (park_chip.isChecked()) {
+                    setPref.add("park_chip");
+                }
+                if (synagogue_chip.isChecked()) {
+                    setPref.add("synagogue_chip");
+                }
             }
         };
+
         museum_chip.setOnCheckedChangeListener(filterChipListener);
         church_chip.setOnCheckedChangeListener(filterChipListener);
         art_gallery_chip.setOnCheckedChangeListener(filterChipListener);
+        aquarium_chip.setOnCheckedChangeListener(filterChipListener);
+        zoo_chip.setOnCheckedChangeListener(filterChipListener);
+        amusement_park_chip.setOnCheckedChangeListener(filterChipListener);
+        city_hall_chip.setOnCheckedChangeListener(filterChipListener);
+        mosque_chip.setOnCheckedChangeListener(filterChipListener);
+        park_chip.setOnCheckedChangeListener(filterChipListener);
+        synagogue_chip.setOnCheckedChangeListener(filterChipListener);
 
-        museum_chip.setChecked(mPreferences.get("museum"));
+        museum_chip.setChecked(true);
     }
 
     public void savePlaces(View view) {
         if (mUserPreferences == null) {
             mUserPreferences = new UserPreferences();
-            //UserPreferences userPreferences = UserPreferences.getInstance();
-            setPref = mPreferences.keySet();
 
             for (String s : setPref) {
                 aPreferences.add(s);
             }
-//
-//            userPreferences.setPreferences(aPreferences);
+
             Log.d(TAG, "Preferences LIST: " + aPreferences.toString());
 
             DocumentReference userRef = mDb.collection(getString(R.string.collection_users))
@@ -104,7 +132,6 @@ public class UserPlacesActivity extends AppCompatActivity {
         }
 
         Intent intent = new Intent(this, ProfileActivity.class);
-
         startActivity(intent);
     }
 
